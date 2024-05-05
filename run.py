fd648569d735b5dc24c748999e610ff76c4f3d5c
Kouta <ishikuro6.2@gmail.com>
2024-05-05 14:02:18 +0900

    ログを残すように実装

--- run.py
+++ run.py
@@ -10,29 +10,32 @@ from sklearn.metrics import mean_squared_error
 import lightgbm as lgb
 
 # ▼親ディレクトリの定義
-BASE_DIR = str(Path(os.path.abspath(''))) + "/"
+BASE_DIR = str(Path(os.path.abspath(''))) 
+LOG_DIR = BASE_DIR + "/logs"
 
 # ▼ロガーの設定
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-# ファイルハンドラの設定
-file_handler = logging.FileHandler(BASE_DIR + 'logs/train_00.log')
-file_handler.setLevel(logging.INFO)
-
-# フォーマッタの設定
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-
-# ロガーにファイルハンドラを追加
-logger.addHandler(file_handler)
-
+# ▼feather形式のファイルを読み込む
 def load_datasets(feats):
-    dfs = [pd.read_feather(f'{BASE_DIR}features/feature_data/{f}_train.feather') for f in feats]
+    dfs = [pd.read_feather(f'{BASE_DIR}/features/feature_data/{f}_train.feather') for f in feats]
     X_train = pd.concat(dfs, axis=1)
-    dfs = [pd.read_feather(f'{BASE_DIR}features/feature_data/{f}_test.feather') for f in feats]
+    dfs = [pd.read_feather(f'{BASE_DIR}/features/feature_data/{f}_test.feather') for f in feats]
     X_test = pd.concat(dfs, axis=1)
     return X_train, X_test
+
+# ▼使用した特徴量・ハイパーパラメータを保存する
+def save_feature_list(run_name: str, features: list, params: dict):
+    with open(f"{LOG_DIR}/{run_name}/{run_name}_features.txt", "w") as f:
+        for ele in features:
+            f.write(ele+"\n")
+        f.close()
+
+    with open(f"{LOG_DIR}/{run_name}/{run_name}_param.txt", "w") as f:
+        for key, value in params.items():
+            f.write(f"{key}:{value}\n")
+        f.close()
 
 
 class HousePricesModel:
@@ -54,12 +57,28 @@ class HousePricesModel:
             テストデータに対してモデルを評価し、評価指標を返す。
     """
 
-    def __init__(self, target_col="SalePrice", n_plits=5):
+    def __init__(self, run_name: str, target_col="SalePrice", n_plits=5):
+        self.run_name = run_name
         self.models = []
         self.valid_scores = []
         self.train_logs = []
         self.target_col = target_col
         self.n_splits = n_plits
+
+        new_dir = f"{LOG_DIR}/{run_name}"
+        if not os.path.exists(new_dir):
+            os.mkdir(new_dir)
+
+        # ファイルハンドラの設定
+        file_handler = logging.FileHandler(f"{LOG_DIR}/{self.run_name}/general.log")
+
+        # フォーマッタの設定
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+
+        # ロガーにファイルハンドラを追加
+        logger.addHandler(file_handler)
+
 
     def train(self, train_data: pd.DataFrame, params: dict, num_boost_round=10000) -> None:
         """学習データを用いてモデルを学習する。
@@ -88,6 +107,7 @@ class HousePricesModel:
             lgb_train = lgb.Dataset(X_tr, y_tr)
             lgb_eval = lgb.Dataset(X_va, y_va, reference=lgb_train)
 
+            logger.info(f"{self.run_name} - start training cv")
             evals_result = {}
             model = lgb.train(
                 self.params,
@@ -109,9 +129,14 @@ class HousePricesModel:
             self.valid_scores.append(score)
 
             # 学習ログの保存
-            for iteration, (train_metric, val_metric) in enumerate(zip(evals_result['train']['l2'], evals_result['valid']['l2']), start=1):
-                    logger.info(f"Fold {fold+1}/{self.n_splits} - Iteration: {iteration}, Train MSE: {train_metric}, Validation MSE: {val_metric}")
-            logger.info(f"Fold {fold+1}/{self.n_splits} **END**")
+            logger.info(f"{self.run_name} - Fold {fold+1}/{self.n_splits} - score {score:.2f}")
+
+
+        # ログの記録
+        logger.info(f"{run_name} - end training cv - score {np.mean(self.valid_scores):.2f}")
+
+        # 使用した特徴量・ハイパーパラメータを保存
+        save_feature_list(self.run_name, X_train.columns, self.models[0].params)
 
             
 
@@ -155,14 +180,17 @@ class HousePricesModel:
     
 if __name__=="__main__":
 
+    run_name = "lgb_001"
+
     # ▼特徴量の指定
     features = {
         "HouseArea",
         "MSSubClass"
     }
 
+    # ▼データの準備
     X_train, X_test = load_datasets(features)
-    y_train = pd.read_csv(BASE_DIR + "data/train.csv")["SalePrice"]
+    y_train = pd.read_csv(BASE_DIR + "/data/train.csv")["SalePrice"]
     train_data = pd.concat([X_train, y_train], axis=1)
 
     # ▼パラメータの設定
@@ -174,16 +202,14 @@ if __name__=="__main__":
         "min_data_in_bin": 3,
         "bagging_freq": 1,
         "bagging_seed": 0,
-        "verbose": -1
+        # "verbose": -1,
     }
 
 
     # ▼モデルの学習
-    model = HousePricesModel()
+    model = HousePricesModel(run_name=run_name)
     model.train(
         train_data,
         params
     )
 
-    # ログの記録
-    logger.info("Model trained successfully")
